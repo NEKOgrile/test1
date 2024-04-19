6ae6302cd0252fc918803b31c34b351bd4496326
willem <willemcornil@gmail.com>
2024-04-19 10:52:04 +0200

    maj

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-landing-page',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+
+  constructor(private router: Router ){}
+
+  ngOnInit() : void{
+
+   
+  }
+
+  onContinue() {
+    this.router.navigateByUrl('facesnaps');
+}
 
 }
